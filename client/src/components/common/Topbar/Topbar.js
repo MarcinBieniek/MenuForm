48693658caa2c820f289b1248dfc69eefceccb9a
Marcin Bieniek <marcingim@gmail.com>
2023-05-04 13:19:45 +0200

    Add Navbar component

--- client/src/components/common/Topbar/Topbar.js
+++ client/src/components/common/Topbar/Topbar.js
@@ -2,9 +2,20 @@ import styles from './Topbar.module.scss'
 
 const Topbar = () => {
   return (
-    <header className={styles.container}>
+    <header className={styles.header}>
       <div className={styles.logo}>
-        Menu.
+        Menu
+        <span className={styles.dot}>.</span>
+      </div>
+      <div className={styles.user}>
+        <img
+        className={styles.image}
+        alt="User photo"
+        src={`${process.env.PUBLIC_URL}/images/gordon.png`} />
+        <div className={styles.username}>
+          <p>Hi, Gordon</p>
+          <p>Chef</p>
+        </div>
       </div>
     </header>
   )
