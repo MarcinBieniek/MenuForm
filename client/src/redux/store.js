474183924c97b879471cb01a35a9b742c0c24d9c
Marcin Bieniek <marcingim@gmail.com>
2023-05-09 10:03:38 +0200

    Build development changes

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -13,7 +13,6 @@ const reducer = combineReducers(subreducers)
 
 const storeEnhancers = compose(
   applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(
